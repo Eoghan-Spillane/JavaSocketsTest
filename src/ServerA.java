8d45152e634e0885159df5374c5a42c79a300f60
Eoghan Spillane <Eoghan.Spillane@mycit.ie>
2020-11-28 15:12:59 +0000

    Server is able to recieve new Lists from clients at any time

--- src/ServerA.java
+++ src/ServerA.java
@@ -20,12 +20,11 @@ public class ServerA implements Runnable{
             InputStream inputStream = socket.getInputStream();
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 
-
-
             //Variables must be created on the server for the incoming objects to be added to it.
             //noinspection unchecked
             List<Car> cars = (List<Car>) objectInputStream.readObject();
             System.out.println("Received [" + cars.size() + "] Cars");
+
             for (Car car : cars) {
                 System.out.println(car.toString());
             }
