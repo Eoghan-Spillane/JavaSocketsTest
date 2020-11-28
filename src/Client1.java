9e86231e933f9eb9d885e4cbde71d73785c21438
Eoghan Spillane <Eoghan.Spillane@mycit.ie>
2020-11-28 15:20:03 +0000

    Implemented TryCatch in case Server isn't running

--- src/Client1.java
+++ src/Client1.java
@@ -1,5 +1,7 @@
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,26 +10,30 @@ import java.util.List;
 
 public class Client1 {
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 4999);
-        System.out.println("Connected");
+    public static void main(String[] args){
+        try{
+            Socket socket = new Socket("localhost", 4999);
+            System.out.println("Connected");
 
-        //Object to be sent must be created first
+            //Object to be sent must be created first
 
-        OutputStream outputStream = socket.getOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Kia", 2006));
-        cars.add(new Car("SangYong", 2011));
-        cars.add(new Car("Samsung", 2003));
-        cars.add(new Car("Mitsubishi", 2003));
+            List<Car> cars = new ArrayList<>();
+            cars.add(new Car("Kia", 2006));
+            cars.add(new Car("SangYong", 2011));
+            cars.add(new Car("Samsung", 2003));
+            cars.add(new Car("Mitsubishi", 2003));
 
-        objectOutputStream.writeObject(cars);
+            objectOutputStream.writeObject(cars);
 
-        System.out.println("Closing Socket");
+            System.out.println("Closing Socket");
 
-        //Forces unsent data to be sent
-        socket.close();
+            //Forces unsent data to be sent
+            socket.close();
+        }catch(IOException e){
+            System.out.println("No Response From Server");
+        }
     }
 }
--- src/Client2.java
+++ src/Client2.java
@@ -5,27 +5,35 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+// https://gist.github.com/chatton/14110d2550126b12c0254501dde73616
+
+
 public class Client2 {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 4998);
-        System.out.println("Connected");
 
-        //Object to be sent must be created first
+    public static void main(String[] args){
+        try{
+            Socket socket = new Socket("localhost", 4998);
+            System.out.println("Connected");
 
-        OutputStream outputStream = socket.getOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            //Object to be sent must be created first
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Ford", 2006));
-        cars.add(new Car("Hyundai", 2011));
-        cars.add(new Car("Toyota", 2003));
-        cars.add(new Car("Toyota", 2007));
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
-        objectOutputStream.writeObject(cars);
+            List<Car> cars = new ArrayList<>();
+            cars.add(new Car("Ford", 2006));
+            cars.add(new Car("Hyundai", 2011));
+            cars.add(new Car("Toyota", 2003));
+            cars.add(new Car("Toyota", 2007));
 
-        System.out.println("Closing Socket");
+            objectOutputStream.writeObject(cars);
 
-        //Forces unsent data to be sent
-        socket.close();
+            System.out.println("Closing Socket");
+
+            //Forces unsent data to be sent
+            socket.close();
+        }catch(IOException e){
+            System.out.println("No Response From Server");
+        }
     }
 }
